cdb75c9b04ce243487c3f01b76ec3dec2b1afdeb
Alexander Stein <alexander.stein@gsa.gov>
2020-05-29 16:54:56 -0400

    Drop a test file to confirm execution.

--- lib/run-scan.sh
+++ lib/run-scan.sh
@@ -3,4 +3,4 @@
 VULS_PATH="${VULS_PATH:-/home/vcap/deps/##INDEX##/depscan/vuls}"
 DEPSCAN_DIR="${DEPSCAN_DIR:-/home/vcap/tmp}"
 
-echo "This is a test on $(date)"
+echo "This is a test on $(date)" >> $DEPSCAN_DIR/vulstest.txt
