98a3d564a029c5b055aa492f11ceed74dddbe530
Alexander Stein <alexander.stein@gsa.gov>
2020-06-02 11:26:15 -0400

    Run scan and post contents via curl.

--- lib/run-scan.sh
+++ lib/run-scan.sh
@@ -1,6 +1,15 @@
 #!/usr/bin/env bash
 
+VULS_SERVER="${VULS_SERVER:-127.0.0.1}"
+VULS_HTTP_SERVER="${VULS_HTTP_SERVER:-http://localhost:5515/vuls}"
 VULS_PATH="${VULS_PATH:-/home/vcap/deps/##INDEX##}"
 RESULTS_DIR="${DEPSCAN_DIR:-/home/vcap/tmp}"
 
-echo "This is a test on $(date)" >> $DEPSCAN_DIR/vulstest.txt
+$VULS_PATH/vuls scan \
+    -config=$VULS_PATH/dist/config.toml
+    -results-dir=$RESULTS_DIR
+
+curl -X POST \
+    -H "Content-Type: application/json" \
+    -d @$RESULTS_DIR/current/localhost.json \
+    "$VULS_HTTP_SERVER"
