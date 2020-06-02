f5ddfce80efba4112813f3232ae1de572935479a
Alexander Stein <alexander.stein@gsa.gov>
2020-06-02 11:45:53 -0400

    Fix obvious var rename error.

--- lib/run-scan.sh
+++ lib/run-scan.sh
@@ -6,7 +6,7 @@ VULS_PATH="${VULS_PATH:-/home/vcap/deps/##INDEX##}"
 VULS_BIN="${VULS_BIN:-\$VULS_PATH/vuls}"
 VULS_SCAN_OPTS="${VULS_SCAN_OPTS:-scan -config=\$VULS_PATH/dist/config.toml -results-dir=\$RESULTS_DIR}"
 UPLOAD_BIN=${UPLOAD_BIN:-/usr/bin/curl}
-UPLOAD_OPTS="${CURL_OPTSt:--X POST -H "Content-Type: application/json" -d @\$RESULTS_DIR/current/localhost.json \$VULS_HTTP_SERVER}"
+UPLOAD_OPTS="$UPLOAD_OPTS:--X POST -H "Content-Type: application/json" -d @\$RESULTS_DIR/current/localhost.json \$VULS_HTTP_SERVER}"
 RESULTS_DIR="${RESULTS_DIR:-/home/vcap/tmp}"
 
 date >> /tmp/vuls_profiled_exec_test.log
