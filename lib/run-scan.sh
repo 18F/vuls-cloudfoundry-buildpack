e6ee8fef5580013b6e492622afcea39ea421aba1
Alexander Stein <alexander.stein@gsa.gov>
2020-06-02 12:31:59 -0400

    Fix overwritten rewrite for buildpack idx path.
    Accidentally copy-pasted over this during debugging code changes
    against cloud.gov staging environment.

--- lib/run-scan.sh
+++ lib/run-scan.sh
@@ -2,7 +2,7 @@
 
 VULS_SERVER="${VULS_SERVER:-127.0.0.1}"
 VULS_HTTP_SERVER="${VULS_HTTP_SERVER:-http://localhost:5515/vuls}"
-VULS_PATH="${VULS_PATH:-/home/vcap/deps/0}"
+VULS_PATH="${VULS_PATH:-/home/vcap/deps/##INDEX##}"
 RESULTS_DIR="${RESULTS_DIR:-/home/vcap/tmp}"
 VULS_BIN="${VULS_BIN:-$VULS_PATH/vuls}"
 VULS_SCAN_OPTS="${VULS_SCAN_OPTS:-scan -config=${VULS_PATH}/dist/config.toml -results-dir=${RESULTS_DIR}}"
