494dc9052f446165699099863669b92b2a9f6f9a
Alexander Stein <alexander.stein@gsa.gov>
2020-06-02 10:30:31 -0400

    Use paths and do not hard-code binary too.

--- lib/run-scan.sh
+++ lib/run-scan.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-VULS_PATH="${VULS_PATH:-/home/vcap/deps/##INDEX##/vuls}"
-DEPSCAN_DIR="${DEPSCAN_DIR:-/home/vcap/tmp}"
+VULS_PATH="${VULS_PATH:-/home/vcap/deps/##INDEX##}"
+RESULTS_DIR="${DEPSCAN_DIR:-/home/vcap/tmp}"
 
 echo "This is a test on $(date)" >> $DEPSCAN_DIR/vulstest.txt
