b43eed9e0b05d5be2b7229b7b80309f23b5f8820
Alexander Stein <alexander.stein@gsa.gov>
2020-06-03 22:54:02 -0400

    Workaround for quote escape and script executable.
    Working around issues with quote escape in default parameters. No matter
    how I quote the default parameter in the parameter substitution causes
    problems no matter what I do.
    
    UPLOAD_OPTS="${UPLOAD_OPTS:--X POST -H "Content-Type: application/json" -d @${RESULTS_DIR}/current/localhost.json ${VULS_HTTP_SERVER}}"
    
    UPLOAD_OPTS="${UPLOAD_OPTS:--X POST -H 'Content-Type: application/json' -d @${RESULTS_DIR}/current/localhost.json ${VULS_HTTP_SERVER}}"
    
    UPLOAD_OPTS="${UPLOAD_OPTS:--X POST -H \'Content-Type: application/json\' -d @${RESULTS_DIR}/current/localhost.json ${VULS_HTTP_SERVER}}"
    
    UPLOAD_OPTS="${UPLOAD_OPTS:--X POST -H \"Content-Type: application/json\" -d @${RESULTS_DIR}/current/localhost.json ${VULS_HTTP_SERVER}}"
    
    None of these prevent from bash breaking the string for the header
    Content-Type and its value into a separate string, as bash indicates
    in debugger mode.
    
    + /usr/bin/curl '"-X"' POST -H '"Content-Type:' 'application/json"' -d @/home/vcap/tmp/current/localhost.json http://1.2.3.4:5515/vuls

--- lib/run-scan.sh
+++ lib/run-scan.sh
@@ -4,10 +4,20 @@ VULS_SERVER="${VULS_SERVER:-127.0.0.1}"
 VULS_HTTP_SERVER="${VULS_HTTP_SERVER:-http://localhost:5515/vuls}"
 VULS_PATH="${VULS_PATH:-/home/vcap/deps/##INDEX##}"
 RESULTS_DIR="${RESULTS_DIR:-/home/vcap/tmp}"
-VULS_BIN="${VULS_BIN:-${VULS_PATH}/vuls}"
-VULS_SCAN_OPTS="${VULS_SCAN_OPTS:-scan -config=${VULS_PATH}/dist/config.toml -results-dir=${RESULTS_DIR}}"
-UPLOAD_BIN=${UPLOAD_BIN:-/usr/bin/curl}
-UPLOAD_OPTS="${UPLOAD_OPTS:--X POST -H 'Content-Type: application/json' -d @${RESULTS_DIR}/current/localhost.json ${VULS_HTTP_SERVER}}"
+SCAN_BIN="${VULS_BIN:-${VULS_PATH}/vuls}"
+SCAN_OPTS="${VULS_SCAN_OPTS:-scan -config=${VULS_PATH}/dist/config.toml -results-dir=${RESULTS_DIR}}"
+UPLOAD_BIN="${UPLOAD_BIN:-''}"
+UPLOAD_OPTS="${UPLOAD_OPTS:-''}"
 
-$VULS_BIN $VULS_SCAN_OPTS
-$UPLOAD_BIN $UPLOAD_OPTS
+$SCAN_BIN $SCAN_OPTS
+
+if [[ -z "$UPLOAD_BIN" ]]; then
+  echo "Uploading with custom '$UPLOAD_BIN' command"
+  $UPLOAD_BIN $UPLOAD_OPTS
+else
+  echo "Uploading with curl default"
+  /usr/bin/curl -X POST \
+    -H 'Content-Type: application/json' \
+    -d @${RESULTS_DIR}/current/localhost.json \
+    ${VULS_HTTP_SERVER}
+fi
